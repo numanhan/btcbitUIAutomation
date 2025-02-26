4c91710ce06b4a94b3a0a6facccc5533cc36fdab
numanhan duran <numanhanduran@gmail.com>
2025-02-26 22:30:20 +0100

    Update ContactsPage.java

--- src/main/java/pages/ContactsPage.java
+++ src/main/java/pages/ContactsPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -13,44 +14,47 @@ import static drivers.Driver.closeDriver;
 public class ContactsPage {
     private WebDriver driver;
     private WebDriverWait wait;
+    private JavascriptExecutor jsExecutor;
 
-    private By pageTitle = By.xpath("//h1[contains(text(), 'Contact Us')]");
-    private By nameInput = By.name("name");
-    private By emailInput = By.name("email");
-    private By messageInput = By.name("message");
-    private By submitButton = By.xpath("//button[contains(text(), 'Send Message')]");
+    private By pageTitle = By.xpath("//h1[contains(text(), 'Contacts')]");
+    private By countryTitle = By.xpath("//h2[contains(text(), 'Poland')]");
+    private By addressTitle = By.xpath("//h3[contains(text(),'BTCBIT Sp. z o.o. Ul. Gesia 8 - 205, 31-535Krakow, Poland']");
+    private By regNumber = By.xpath("//h4[contains(text(),'Reg. Nr.']");
+
 
     public ContactsPage(WebDriver driver) {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        this.jsExecutor = (JavascriptExecutor) driver;
+    }
+
+    private void scrollPage(int pixels) {
+        jsExecutor.executeScript("window.scrollBy(0, " + pixels + ");");
     }
 
     public boolean isPageTitleDisplayed() {
+        scrollPage(300); 
         WebElement titleElement = wait.until(ExpectedConditions.visibilityOfElementLocated(pageTitle));
         return titleElement.isDisplayed();
     }
 
-    public boolean isNameInputDisplayed() {
-        WebElement nameElement = wait.until(ExpectedConditions.visibilityOfElementLocated(nameInput));
-        return nameElement.isDisplayed();
+    public boolean isCountryTitleDisplayed() {
+        WebElement country = wait.until(ExpectedConditions.visibilityOfElementLocated(countryTitle));
+        return country.isDisplayed();
     }
 
-    public boolean isEmailInputDisplayed() {
-        WebElement emailElement = wait.until(ExpectedConditions.visibilityOfElementLocated(emailInput));
-        return emailElement.isDisplayed();
+    public boolean isAddressTitleDisplayed() {
+        WebElement address = wait.until(ExpectedConditions.visibilityOfElementLocated(addressTitle));
+        return address.isDisplayed();
     }
 
-    public boolean isMessageInputDisplayed() {
-        WebElement messageElement = wait.until(ExpectedConditions.visibilityOfElementLocated(messageInput));
-        return messageElement.isDisplayed();
+    public boolean isRegNumberDisplayed() {
+        WebElement regNum = wait.until(ExpectedConditions.visibilityOfElementLocated(regNumber));
+        return regNum.isDisplayed();
     }
 
-    public boolean isSubmitButtonDisplayed() {
-        WebElement submitElement = wait.until(ExpectedConditions.visibilityOfElementLocated(submitButton));
-        return submitElement.isDisplayed();
-    }
 
-    public void close_driver(){
+    public void close_driver() {
         closeDriver();
     }
 }
